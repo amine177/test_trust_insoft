4b495985b40a8c45a64009d800d805f676ae1193
amine177 <amri.amine@protonmail.com>
2021-04-05 19:44:43 +0200

    added a check to insure that the code is well defined

--- main.c
+++ main.c
@@ -5,8 +5,9 @@ int main(int argc, char** argv)
 	//while (argc > -5)
 	//	printf(argv[argc--]);
 	if (1)
-		if (*argv[1] & 0xc0)
-			if (*argv[*argv[1]] &0xc0)
+		if (argc >= 1)
+			if (*argv[1] & 0xc0)
+				if (*argv[*argv[1]] &0xc0)
 				printf("done\n");
 	return 0;
 }
