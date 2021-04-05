a2b1acad5659674b0d31198d2df18df2b66e7583
amine177 <amri.amine@protonmail.com>
2021-04-05 19:33:57 +0200

    remove undefined behaviour

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char** argv)
 {
-	while (argc > -5)
-		printf(argv[argc--]);
+	//while (argc > -5)
+	//	printf(argv[argc--]);
 	printf(argv[argc & ~(argc) - argc]);
 	return 0;
 }
