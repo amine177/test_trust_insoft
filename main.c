547411699ae3eed1cb9091a86c87592f05a319bc
amine177 <amri.amine@protonmail.com>
2021-04-06 11:17:44 +0200

    added a sanity check

--- main.c
+++ main.c
@@ -4,8 +4,9 @@ int main(int argc, char** argv)
 {
 	//while (argc > -5)
 	//	printf(argv[argc--]);
+	printf("%d\n", argc);
 	if (1)
-		if (argc >= 1)
+		if (argc > 1)
 			if (argv)
 				if ((char)*argv[1] & 0xc0)
 					if ((char)*argv[(char)*argv[1]] &0xc0)
